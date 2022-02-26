bea2e6d80f485fd74938ba0dcfd436426bcc7331
nucastcle <panuwat.ch@castcle.com>
2022-02-26 19:04:38 +0700

    cron

--- apps/tutorials/src/app/app.module.ts
+++ apps/tutorials/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
 
 import { ValidatesController } from './controllers/validates.controller';
+import { CronService } from './service/cron.service';
 
 @Module({
-  imports: [],
+  imports: [ScheduleModule.forRoot()],
   controllers: [ValidatesController],
-  providers: [],
+  providers: [CronService],
 })
 export class AppModule {}
---
+++ apps/tutorials/src/app/service/cron.service.ts
@@ -0,0 +1,13 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { Cron } from '@nestjs/schedule';
+
+@Injectable()
+export class CronService {
+  private readonly logger = new Logger(CronService.name);
+  @Cron('* * * * * *', {
+    name: 'cron',
+  })
+  trigger() {
+    this.logger.debug('run!!');
+  }
+}
